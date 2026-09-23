bb223d9c67d8d4023238e4736caece70138601cf
soh62164-collab <soh62164@gmail.com>
2026-09-24 02:55:42 +0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+var V="hadith-v1";
+var SHELL=["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png"];
+self.addEventListener("install",function(e){
+  e.waitUntil(caches.open(V).then(function(c){return c.addAll(SHELL)}));
+  self.skipWaiting();
+});
+self.addEventListener("activate",function(e){
+  e.waitUntil(caches.keys().then(function(k){
+    return Promise.all(k.filter(function(x){return x!==V}).map(function(x){return caches.delete(x)}));
+  }));
+  self.clients.claim();
+});
+self.addEventListener("fetch",function(e){
+  if(e.request.method!=="GET")return;
+  e.respondWith(
+    caches.match(e.request).then(function(hit){
+      var net=fetch(e.request).then(function(r){
+        if(r&&r.ok){var cp=r.clone();caches.open(V).then(function(c){c.put(e.request,cp)})}
+        return r;
+      }).catch(function(){return hit});
+      return hit||net;
+    })
+  );
+});
